57ffa90df513e701829eccf2e3f92f4842c31cf8
jordandeeds31 <33381472+jordandeeds31@users.noreply.github.com>
2026-04-12 15:25:37 -0400

    modified game.service.ts to return data from mlb api if data is not cached in redis so frontend can fetch data immediately on component mount

--- backend/modules/game/game.service.ts
+++ backend/modules/game/game.service.ts
@@ -1,12 +1,18 @@
 import { cacheService } from "../../integrations/redis/redis.service";
+import { mlbApiService } from "../../integrations/mlb/mlb.api.service";
+import transformGames from "./game.transform";
 
 export const gameService = {
   getTodaysGames: async () => {
     const date = new Date().toLocaleDateString("en-CA");
-    console.log(date);
     const cacheKey = `games:mlb:${date}`;
+
     const cached = await cacheService.get(cacheKey);
     if (cached) return cached;
-    return [];
+
+    const rawGames = await mlbApiService.getTodaysGames();
+    const games = transformGames(rawGames);
+    await cacheService.set(cacheKey, games, 120);
+    return games;
   },
 };
